6721b43d4e2ed6b965725acafcaf367ec5f3d487
Jessica <jessica.homet@gmail.com>
2019-05-01 15:18:20 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,22 @@
 Rails.application.routes.draw do
   root to: "homepages#index"
 
-  get "homepages/index"
-  get "reviews/new"
-  get "reviews/create"
-  get "categories/new"
-  get "categories/create"
-  get "orders/index"
-  get "orders/show"
-  get "orders/create"
-  get "orders/destroy"
-  get "orders/edit"
-  get "orders/update"
-  get "orders/submit"
-  get "orders/cust_info"
-  get "orderitems/edit"
-  get "orderitems/update"
-  get "products/index"
-  get "products/show"
-  get "products/new"
-  get "products/create"
-  get "products/edit"
-  get "products/update"
-  get "products/retire"
-  get "users/index"
-  get "users/show"
-  get "users/login"
-  get "users/logout"
+  resources :reviews, only: [:new, :create] # might not need new
+
+  resources :categories, only: [:new, :create, :show]
+
+  resources :orders, except: [:new] # might not need index
+
+  resources :orderitems, only: [:edit, :update, :destroy] # clarify this more later
+
+  get "orders/cust_info", to: "orders#cust_info", as: "cust_info"
+  patch "orders/submit", to: "orders#submit", as: "submit"
+
+  resources :products, except: [:destroy]
+  get "/products/retire", to: "products#retire", as: "retire"
+
+  resources :users, only: [:index, :show]
+
+  get "users/login" # authentication stuff... do this later
+  get "users/logout", to: "users#logout", as: "logout"
 end
