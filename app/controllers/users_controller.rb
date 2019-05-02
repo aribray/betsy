60572f24f72a295b4c34b0e2828ff9c48f507682
Ariana Bray <aribray@gmail.com>
2019-05-02 14:12:25 -0700

    added index and show actions and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      flash[:error] = "Could not find user with id: #{params[:id]}"
+      redirect_to root_path, status: 302
+    end
   end
 
-  def login
-  end
+  def login; end
 
-  def logout
-  end
+  def logout; end
 
   def myaccount
     # this will be the current logged in user's merchant dashboard
@@ -17,5 +23,11 @@ class UsersController < ApplicationController
 
   def orders
     # this will be the current logged in user's orders, if we want that as its own page
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :uid, :provider, :name)
   end
 end
