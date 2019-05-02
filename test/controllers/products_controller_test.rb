bd5c28d25ced4ce489011bdacbc2f95485dc330e
Mina Shin <minamishin@hotmail.com>
2019-05-02 02:36:32 -0700

    adding index page for all products and index test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,38 +2,38 @@ require "test_helper"
 
 describe ProductsController do
   it "should get index" do
-    get products_index_url
-    value(response).must_be :success?
+    get products_path
+
+    must_respond_with :success
   end
 
-  it "should get show" do
-    get products_show_url
-    value(response).must_be :success?
-  end
+  # it "should get show" do
+  #   get products_show_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get new" do
-    get products_new_url
-    value(response).must_be :success?
-  end
+  # it "should get new" do
+  #   get products_new_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get create" do
-    get products_create_url
-    value(response).must_be :success?
-  end
+  # it "should get create" do
+  #   get products_create_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get edit" do
-    get products_edit_url
-    value(response).must_be :success?
-  end
+  # it "should get edit" do
+  #   get products_edit_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get update" do
-    get products_update_url
-    value(response).must_be :success?
-  end
+  # it "should get update" do
+  #   get products_update_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get retire" do
-    get products_retire_url
-    value(response).must_be :success?
-  end
-
+  # it "should get retire" do
+  #   get products_retire_url
+  #   value(response).must_be :success?
+  # end
 end
