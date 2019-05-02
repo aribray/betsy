ed568c48f675db311eda7ec7d9b83b79af580e81
Jessica <jessica.homet@gmail.com>
2019-05-01 17:12:45 -0700

    Add show controller test and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_002317) do
+ActiveRecord::Schema.define(version: 2019_05_02_000804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_05_01_002317) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_categories_products_on_category_id"
+    t.index ["product_id"], name: "index_categories_products_on_product_id"
   end
 
   create_table "orderitems", force: :cascade do |t|
@@ -55,13 +62,6 @@ ActiveRecord::Schema.define(version: 2019_05_01_002317) do
     t.boolean "retired", default: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_products_on_user_id"
-  end
-
-  create_table "products_categories_joins", force: :cascade do |t|
-    t.bigint "product_id"
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_products_categories_joins_on_category_id"
-    t.index ["product_id"], name: "index_products_categories_joins_on_product_id"
   end
 
   create_table "reviews", force: :cascade do |t|
