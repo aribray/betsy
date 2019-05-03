95ba5d737ec6071d4e139301e78a42dc80a7f329
Jessica <jessica.homet@gmail.com>
2019-05-03 11:51:26 -0700

    controller and test for orders#show

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,44 +1,59 @@
 require "test_helper"
 
 describe OrdersController do
-  it "should get index" do
-    get orders_index_url
-    value(response).must_be :success?
+  # it "should get index" do
+  #   get orders_index_url
+  #   value(response).must_be :success?
+  # end
+
+  describe "show" do
+    it "should get show" do
+      get orders_path
+      value(response).must_be :success?
+    end
+
+    it "should show a list of products sold by the user" do
+      order = orders(:one)
+      get order_path(order.id)
+      must_respond_with :success
+    end
+
+    it "will redirect if we try to view products from an invalid user" do
+      get order_path(-1)
+
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "Could not find order with id: -1"
+    end
   end
 
-  it "should get show" do
-    get orders_show_url
-    value(response).must_be :success?
-  end
+  # it "should get create" do
+  #   get orders_create_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get create" do
-    get orders_create_url
-    value(response).must_be :success?
-  end
+  # it "should get destroy" do
+  #   get orders_destroy_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get destroy" do
-    get orders_destroy_url
-    value(response).must_be :success?
-  end
+  # it "should get edit" do
+  #   get orders_edit_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get edit" do
-    get orders_edit_url
-    value(response).must_be :success?
-  end
+  # it "should get update" do
+  #   get orders_update_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get update" do
-    get orders_update_url
-    value(response).must_be :success?
-  end
+  # it "should get submit" do
+  #   get orders_submit_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get submit" do
-    get orders_submit_url
-    value(response).must_be :success?
-  end
-
-  it "should get cust_info" do
-    get orders_cust_info_url
-    value(response).must_be :success?
-  end
+  # it "should get cust_info" do
+  #   get orders_cust_info_url
+  #   value(response).must_be :success?
+  # end
 
 end
