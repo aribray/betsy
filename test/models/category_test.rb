ec043761f5d544b999f17da3305291269cc0dffd
Mina Shin <minamishin@hotmail.com>
2019-05-06 16:49:36 -0700

    completed category model tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -29,63 +29,49 @@ describe Category do
   end
 
   describe "relationships" do
-    before do
-      @product = Product.create(name: "amazing product", user: user, price: 10)
-      @new_product = Product.create(name: "new amazing product", user: user, price: 10)
-    end
+    let (:product) {
+      Product.create(name: "amazing product", user: user, price: 10)
+    }
+    let (:new_product) {
+      Product.create(name: "new amazing product", user: user, price: 10)
+    }
+
     it "has many products" do
-      category.products << @product
+      category.products << product
       category.save
 
       new_category = Category.find_by(id: category.id)
 
-      expect(new_category.products).must_include @product
+      expect(new_category.products).must_include product
       expect(new_category.products.length).must_equal 1
 
-      category.products << @new_product
+      category.products << new_product
       category.save
 
       another_category = Category.find_by(id: category.id)
-      expect(another_category.products).must_include @new_product
+      expect(another_category.products).must_include new_product
       expect(another_category.products.length).must_equal 2
     end
 
     it "can belong to many products" do
       category_two = categories(:two)
-      category_two.products << @product
+      category_two.products << product
       category_two.save
 
-      category.products << @product
+      category.products << product
       category.save
 
-      product = Product.find_by(id: @product.id)
-      expect(@product.categories).must_include category_two
-      expect(@product.categories).must_include category
+      this_product = Product.find_by(id: product.id)
+
+      expect(this_product.categories).must_include category_two
+      expect(this_product.categories).must_include category
+      expect(this_product.categories.length).must_equal 2
+    end
+
+    it "can have 0 categories" do
+      categories = product.categories
+
+      expect(categories.length).must_equal 0
     end
   end
-  #     it "can have 0 categories" do
-  #       categories = product.categories
-
-  #       expect(categories.length).must_equal 0
-  #     end
-
-  #     it "can have 1 or more categories by shoveling a category into product.categories" do
-  #       new_category = categories(:one)
-
-  #       product.categories << new_category
-
-  #       expect(new_category.products).must_include product
-  #     end
-  #   end
 end
-# it "is valid when all required fields are present" do
-#   category = categories(:fun)
-#   valid_category = category.valid?
-#   expect(valid_category).must_equal true
-# end
-
-# it "must be invalid if name is missing" do
-#   new_category = Category.new(name: "FunFunFun")
-#   valid_category = new_category.valid?
-#   expect(new_category).must_equal false
-# end
