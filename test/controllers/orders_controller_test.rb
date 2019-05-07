c61dbc18e03e2dcfe192763872202849e1ce98a7
amyesh <amyesh08@gmail.com>
2019-05-07 09:29:38 -0700

    submit methods tests etc

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -26,35 +26,105 @@ describe OrdersController do
     end
   end
 
-  # it "should get create" do
-  #   get orders_create_url
-  #   value(response).must_be :success?
-  # end
+  describe "checkout" do
+    it "should get checkout page" do
+      get checkout_path
 
-  # it "should get destroy" do
-  #   get orders_destroy_url
-  #   value(response).must_be :success?
-  # end
+      must_respond_with :success
+    end
+  end
 
-  # it "should get edit" do
-  #   get orders_edit_url
-  #   value(response).must_be :success?
-  # end
+  describe "confirmation" do
+    it "if given valid params, changes status to paid" do
+      #redirects to confirmation and returns success
+      # input_cc_name = "Divine"
+      # input_cc_number = 1234556
+      # input_cvv = 123
+      # input_expiration_date = 1220
+      # input_address = "555 Scammer Lane"
+      # input_zipcode = 98105
+      # input_email = "divine@scams.net"
+      # update_input = {
+      #   "order": {
+      #     cc_name: input_cc_name,
+      #     cc_number: input_cc_number,
+      #     cvv: input_cvv,
+      #     address: input_address,
+      #     zipcode: input_zipcode,
+      #     email: input_email,
+      #   },
+      # }
 
-  # it "should get update" do
-  #   get orders_update_url
-  #   value(response).must_be :success?
-  # end
+      # get confirmation_path, params: update_input
 
-  # it "should get submit" do
-  #   get orders_submit_url
-  #   value(response).must_be :success?
-  # end
+      # expect(@current_order.cc_name).must_equal "Divine"
+      # expect(@current_order.status).must_equal :paid
+      # must_respond_with :success
+    end
+    # it "should get confirmation page" do
+    #   get confirmation_path
 
-  # it "should get cust_info" do
-  #   get orders_cust_info_url
-  #   value(response).must_be :success?
-  # end
+    #   must_respond_with :success
+    # end
+    # it "should give a 400 error if given invalid params"
+    # input_cc_name = "Divine"
+    # input_cc_number = 1234556
+    # input_cvv = nil
+    # input_expiration_date = 1220
+    # input_address = "555 Scammer Lane"
+    # input_zipcode = 98105
+    # input_email = "divine@scams.net"
+    # update_input = {
+    #   "order": {
+    #     cc_name: input_cc_name,
+    #     cc_number: input_cc_number,
+    #     cvv: input_cvv,
+    #     address: input_address,
+    #     zipcode: input_zipcode,
+    #     email: input_email,
+    #   },
+    # }
+    # expect do
+    #   patch submit_path(@current_order.id), params: update_input
+    # end.wont_change "Order.count"
+    # must_respond_with :bad_request
+    # expect(@current_order.status).must_equal :paid
+    # end
+  end
+
+  describe "submit" do
+    it "will give a 400 error with invalid params" do
+      # input_cc_name = "Divine"
+      # input_cc_number = 1234556
+      # input_cvv = nil
+      # input_expiration_date = 1220
+      # input_address = "555 Scammer Lane"
+      # input_zipcode = 98105
+      # input_email = "divine@scams.net"
+      # update_input = {
+      #   "order": {
+      #     cc_name: input_cc_name,
+      #     cc_number: input_cc_number,
+      #     cvv: input_cvv,
+      #     address: input_address,
+      #     zipcode: input_zipcode,
+      #     email: input_email,
+      #   },
+      # }
+      # expect do
+      #   patch submit_path(@current_order.id), params: update_input
+      # end.wont_change "Order.count"
+      # must_respond_with :bad_request
+      # expect(@current_order.status).must_equal :paid
+    end
+  end
+
+  describe "destroy" do
+    it "deletes current order" do
+    end
+    it "sets session order id to nil and redirects" do
+    end
+  end
 
   describe "empty_order" do
     it "should get empty_order" do
