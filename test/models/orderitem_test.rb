52d3cff31ad514c57da786f8136885b67bacaa97
Mina Shin <minamishin@hotmail.com>
2019-05-07 16:57:39 -0700

    added dependency tests for orderitem model

--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe Orderitem do
-  let(:orderitem) { Orderitem.new }
+  let(:orderitem) { orderitems(:three) }
 
-  # it "must be valid" do
-  #   value(orderitem).must_be :valid?
-  # end
+  describe "validations" do
+    it "must contain quantity as a number" do
+      orderitem.quantity = nil
+      valid_orderitem = orderitem.valid?
+
+      expect(valid_orderitem).must_equal false
+      expect(orderitem.errors.messages).must_include :quantity
+      expect(orderitem.errors.messages[:quantity]).must_equal ["is not a number"]
+    end
+
+    it "must have a number quantity greater than 0" do
+      orderitem.quantity = -1
+      valid_orderitem = orderitem.valid?
+
+      expect(valid_orderitem).must_equal false
+      expect(orderitem.errors.messages).must_include :quantity
+      expect(orderitem.errors.messages[:quantity]).must_equal ["must be greater than 0"]
+    end
+  end
 end
