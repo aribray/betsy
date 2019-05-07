ecabf224a6b61aaa175093f48e702db4d37646d3
Mina Shin <minamishin@hotmail.com>
2019-05-07 16:24:36 -0700

    completed most of order model test. keeping validation test for maybe later

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,23 @@
 require "test_helper"
 
 describe Order do
-  let(:order) { Order.new }
+  let(:order) { orders(:one) }
 
-  # it "must be valid" do
-  #   value(order).must_be :valid?
-  # end
+  describe "relationships" do
+    it "has many orderitems" do
+      my_order = order.orderitems
+
+      expect(my_order.length).must_equal 2
+      expect(my_order).must_include orderitems(:three)
+    end
+
+    it "has many products" do
+      my_order = order.products
+
+      expect(my_order.length).must_equal 2
+      expect(my_order).must_include products(:turtleneck)
+    end
+  end
+
+  # we might want to test validations for Orders here
 end
