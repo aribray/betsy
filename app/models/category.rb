00883c255ee7a087232bdf2ce56165f07fa0e476
Ariana Bray <aribray@gmail.com>
2019-05-08 16:39:56 -0700

    fixed new category in new product form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
 
+  # attr_accessor :skip_name_validation
   validates :name, presence: true, uniqueness: true
+  # , unless: :skip_name_validation
+  
 end
