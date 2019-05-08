9801e449f588f5e7b052373c32583d2652609a75
Mina Shin <minamishin@hotmail.com>
2019-05-08 02:26:36 -0700

    completed model tests for orderitem

--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -13,13 +13,53 @@ describe Orderitem do
       expect(orderitem.errors.messages[:quantity]).must_equal ["is not a number"]
     end
 
-    it "must have a number quantity greater than 0" do
+    it "must have a quantity greater than 0" do
       orderitem.quantity = -1
       valid_orderitem = orderitem.valid?
 
       expect(valid_orderitem).must_equal false
       expect(orderitem.errors.messages).must_include :quantity
       expect(orderitem.errors.messages[:quantity]).must_equal ["must be greater than 0"]
+    end
+  end
+
+  describe "relationships" do
+    let (:product) {
+      products(:turtleneck)
+    }
+
+    let (:order) {
+      orders(:one)
+    }
+
+    it "belongs to product" do
+      orderitem.product = product
+
+      expect(orderitem.product_id).must_equal product.id
+      expect(orderitem).must_respond_to :product
+    end
+
+    it "belongs to order" do
+      orderitem.order = order
+
+      expect(orderitem.order_id).must_equal order.id
+      expect(orderitem).must_respond_to :order
+    end
+
+    it "will not change the product count when destroyed" do
+      my_orderitem = orderitem
+
+      expect {
+        my_orderitem.destroy
+      }.wont_change "Product.count"
+    end
+
+    it "will not change the order count when destroyed" do
+      my_orderitem = orderitem
+
+      expect {
+        my_orderitem.destroy
+      }.wont_change "Order.count"
     end
   end
 end
