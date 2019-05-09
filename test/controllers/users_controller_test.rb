912e499110bdb754645b3ca00fc7f8e2def45d11
Ariana Bray <aribray@gmail.com>
2019-05-09 16:03:27 -0700

    fixed merge error in myorders.html.erb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,15 +1,17 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 describe UsersController do
-  describe "index" do
-    it "should get index" do
+  describe 'index' do
+    it 'should get index' do
       get users_path
       must_respond_with :success
     end
   end
 
-  describe "show" do
-    it "should show a list of products sold by the user" do
+  describe 'show' do
+    it 'should show a list of products sold by the user' do
       user = users(:dee)
 
       get user_path(user.id)
@@ -17,16 +19,16 @@ describe UsersController do
       must_respond_with :success
     end
 
-    it "will redirect if we try to view products from an invalid user" do
+    it 'will redirect if we try to view products from an invalid user' do
       get user_path(-1)
 
       must_respond_with :redirect
-      expect(flash[:error]).must_equal "Could not find user with id: -1"
+      expect(flash[:error]).must_equal 'Could not find user with id: -1'
     end
   end
 
-  describe "login" do
-    it "can log in an existing user" do
+  describe 'login' do
+    it 'can log in an existing user' do
       user_count = User.count
 
       user = perform_login
@@ -38,11 +40,11 @@ describe UsersController do
       expect(session[:user_id]).must_equal user.id
     end
 
-    it "can log in a new user" do
-      user = User.new(provider: "github", name: "bob", username: "bobbi", uid: 987, email: "bob@hope.com")
-      expect {
+    it 'can log in a new user' do
+      user = User.new(provider: 'github', name: 'bob', username: 'bobbi', uid: 987, email: 'bob@hope.com')
+      expect do
         perform_login(user)
-      }.must_change "User.count", 1
+      end.must_change 'User.count', 1
 
       expect(flash[:success]).must_equal "Logged in as new user #{user.name}"
       user = User.find_by(uid: user.uid, provider: user.provider)
@@ -50,19 +52,19 @@ describe UsersController do
       expect(session[:user_id]).must_equal user.id
     end
 
-    it "will redirect back to root with a flash message if not coming from github" do
-      user = User.new(provider: "github", name: "bob", username: "bobbi", uid: 987, email: "bob@hope.com")
+    it 'will redirect back to root with a flash message if not coming from github' do
+      user = User.new(provider: 'github', name: 'bob', username: 'bobbi', uid: 987, email: 'bob@hope.com')
 
       start_count = User.count
 
       mock_auth_hash = {
-        provider: "not-github",
+        provider: 'not-github',
         uid: user.uid,
         info: {
           email: user.email,
           nickname: user.username,
-          name: user.name,
-        },
+          name: user.name
+        }
       }
 
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash)
@@ -75,22 +77,22 @@ describe UsersController do
       must_redirect_to root_path
 
       expect(User.count).must_equal start_count
-      expect(flash[:error]).must_equal "Could not create new user account"
+      expect(flash[:error]).must_equal 'Could not create new user account'
     end
 
-    it "will redirect back to root with a flash message if incomplete info is sent" do
-      user = User.new(provider: "github", name: "bob", username: "bobbi", uid: 987, email: "bob@hope.com")
+    it 'will redirect back to root with a flash message if incomplete info is sent' do
+      user = User.new(provider: 'github', name: 'bob', username: 'bobbi', uid: 987, email: 'bob@hope.com')
 
       start_count = User.count
 
       mock_auth_hash = {
-        provider: "github",
+        provider: 'github',
         uid: user.uid,
         info: {
           email: user.email,
           nickname: nil,
-          name: user.name,
-        },
+          name: user.name
+        }
       }
 
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash)
@@ -103,51 +105,51 @@ describe UsersController do
       must_redirect_to root_path
 
       expect(User.count).must_equal start_count
-      expect(flash[:error]).must_equal "Could not create new user account"
+      expect(flash[:error]).must_equal 'Could not create new user account'
     end
   end
 
-  describe "logout" do
-    it "logs out the current user" do
+  describe 'logout' do
+    it 'logs out the current user' do
       user = users(:dee)
       perform_login(user)
       delete logout_path(user)
-      expect(flash[:success]).must_equal "Successfully logged out!"
+      expect(flash[:success]).must_equal 'Successfully logged out!'
       must_respond_with :redirect
     end
   end
 
-  describe "myaccount" do
-    it "should get myaccount if logged in" do
+  describe 'myaccount' do
+    it 'should get myaccount if logged in' do
       perform_login
 
       get myaccount_path
       must_respond_with :success
     end
 
-    it "should redirect to homepage and flash error if not logged in" do
+    it 'should redirect to homepage and flash error if not logged in' do
       get myaccount_path
       must_respond_with :redirect
       must_redirect_to root_path
 
-      flash[:error].must_equal "You must be logged in to do that."
+      flash[:error].must_equal 'You must be logged in to do that.'
     end
   end
 
-  describe "myorders" do
-    it "should get myorders if logged in" do
+  describe 'myorders' do
+    it 'should get myorders if logged in' do
       perform_login
 
       get myorders_path
       must_respond_with :success
     end
 
-    it "should redirect to homepage and flash error if not logged in" do
+    it 'should redirect to homepage and flash error if not logged in' do
       get myorders_path
       must_respond_with :redirect
       must_redirect_to root_path
 
-      flash[:error].must_equal "You must be logged in to do that."
+      flash[:error].must_equal 'You must be logged in to do that.'
     end
   end
 end
